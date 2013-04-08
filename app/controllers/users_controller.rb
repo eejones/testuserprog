02b89c78a5ea20af1fa426bfd88f1bf409da5b4b
Eric Jones <joneserice@gmail.com>
2013-04-07 23:45:51 -0500

    Additions to now allow users to sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
   def show
     @user=User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to this user login program!"
+      #user_url can be replace with @user below
+      redirect_to @user
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
 end
